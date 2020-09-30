81193ca71e6001cc5c7c032aa559a60a86473001
xianengqi <xianengqi@gmail.com>
2020-10-01 01:29:06 +0800

    Initial Commit

---
+++ Sample-DesignCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample-DesignCode
+//
+//  Created by 夏能啟 on 2020/10/1.
+//  Copyright © 2020 夏能啟. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
