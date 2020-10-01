5edd553dcefa4a8b4b20ac6ee9de14e8a50aa431
xianengqi <xianengqi@gmail.com>
2020-10-02 02:01:59 +0800

    feat: 添加动画和手势

--- Sample-DesignCode/ContentView.swift
+++ Sample-DesignCode/ContentView.swift
@@ -9,39 +9,116 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    @State var viewState = CGSize.zero
+    @State var showCard = false
+    @State var bottomState = CGSize.zero
+    @State var showFull = false
+    
     var body: some View {
         ZStack {
-            VStack {
-                Spacer()
-            }
-            .frame(width: 300, height: 220)
-            .background(/*@START_MENU_TOKEN@*/Color.blue/*@END_MENU_TOKEN@*/)
-            .cornerRadius(20)
-            .shadow(radius: 20)
-            .offset(x: 0, y: -20)
+            TitleView()
+                .blur(radius: show ? 20 : 0)
+                .opacity(showCard ? 0.4 : 1)
+                .offset(y: showCard ? -200 : 0)
+                .animation(
+                    Animation
+                        .default
+                        .delay(0.1)
+//                        .speed(2)
+//                        .repeatForever(autoreverses: true)
+                    
+            )
             
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("UI Design")
-                            .font(.title)
-                            .fontWeight(.medium)
-                            .foregroundColor(.white)
-                        Text("Certificate")
-                            .foregroundColor(Color("accent"))
-                    }
-                    Spacer()
-                    Image("Logo1")
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 20)
-                Spacer()
-                Image("Card1").resizable().aspectRatio(contentMode: .fill).frame(width: 300, height: 100, alignment: .top)
+            BackCardView()
+                .frame(width: showCard ? 300 : 340, height: 220)
+                .background(show ? Color("card3") : Color("card4"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -400 : -40)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -180 : 0)
+                .scaleEffect(showCard ? 1 : 0.9)
+                .rotationEffect(.degrees(show ? 0 : 10))
+                .rotationEffect(Angle(degrees: showCard ? -10 : 0))
+                .rotation3DEffect(Angle(degrees: showCard ? 0 : 10), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.5))
+            
+            BackCardView()
+                .frame(width: 340, height: 220)
+                .background(show ? Color("card4") : Color("card3"))
+                .cornerRadius(20)
+                .shadow(radius: 20)
+                .offset(x: 0, y: show ? -200 : -20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -140 : 0)
+                .scaleEffect(showCard ? 1 : 0.95)
+                .rotationEffect(Angle.degrees(show ? 0 : 5))
+                .rotationEffect(Angle(degrees: showCard ? -5 : 0))
+                .rotation3DEffect(Angle(degrees: showCard ? 0 : 5), axis: (x: 10.0, y: 0, z: 0))
+                .blendMode(.hardLight)
+                .animation(.easeInOut(duration: 0.3))
+            
+            CardView()
+                .frame(width: showCard ? 375 : 340.0, height: 220.0)
+                .background(Color.black)
+//                .cornerRadius(20)
+                .clipShape(RoundedRectangle(cornerRadius: showCard ? 30 : 20, style: .continuous))
+                .shadow(radius: 20)
+                .offset(x: viewState.width, y: viewState.height)
+                .offset(y: showCard ? -120 : 0)
+                .blendMode(.hardLight)
+                .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                    self.showCard.toggle()
             }
-            .frame(width: 340.0, height: 220.0)
-            .background(Color.black)
-            .cornerRadius(20)
-            .shadow(radius: 20)
+                // 在gesture之前添加动画和offset，不会影响滞后
+            .gesture(
+                // 添加拖拽手势
+                DragGesture().onChanged { value in
+                    self.viewState = value.translation
+                    self.show = true
+                }
+                .onEnded{ value in
+                    self.viewState = .zero
+                    self.show = false
+                }
+            )
+            
+//            Text("\(bottomState.height)").offset(y: -300)
+            
+            BottomCardView()
+                .offset(x: 0, y: showCard ? 390 : 1000)
+                .offset(y: bottomState.height)
+                .blur(radius: show ? 20 : 0)
+                .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            .gesture(
+                DragGesture().onChanged { value in
+                    self.bottomState = value.translation
+                    if self.showFull {
+                        self.bottomState.height += -300
+                    }
+                    if self.bottomState.height < -300 {
+                        self.bottomState.height = -300
+                    }
+                    
+                }
+                    // 重置的操作
+                .onEnded { value in
+                    if self.bottomState.height > 50 {
+                        self.showCard = false
+                    }
+                    if (self.bottomState.height < -100 && !self.showFull) || (self.bottomState.height < -250 && self.showFull) {
+                        self.bottomState.height = -300
+                        self.showFull = true
+                    } else {
+                        self.bottomState = .zero
+                        self.showFull = false
+                    }
+                    
+                }
+            )
         }
     
     }
@@ -50,5 +127,78 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("UI Design")
+                        .font(.title)
+                        .fontWeight(.medium)
+                        .foregroundColor(.white)
+                    Text("Certificate")
+                        .foregroundColor(Color("accent"))
+                }
+                Spacer()
+                Image("Logo1")
+            }
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
+            Spacer()
+            Image("Card1").resizable().aspectRatio(contentMode: .fill).frame(width: 300, height: 100, alignment: .top)
+        }
+        
+    }
+}
+
+struct BackCardView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+        }
+        
+        
+    }
+}
+
+struct TitleView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Certificate")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .padding()
+            Image("Background1")
+            Spacer()
+        }
+    }
+}
+
+struct BottomCardView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Rectangle()
+                .frame(width: 40, height: 5)
+                .cornerRadius(3)
+                .opacity(0.1)
+            Text("This certificate is proof that Meng To has achieved the UI Design course with approval from a Design+Code instructor.")
+                .multilineTextAlignment(.center)
+                .font(.subheadline)
+                .lineSpacing(4)
+            Spacer()
+        }
+        .padding(.top, 8)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 20)
+        
     }
 }
