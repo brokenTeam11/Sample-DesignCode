269c389d5c8eeb39e77da03a745462f70508472e
xianengqi <xianengqi@gmail.com>
2020-10-05 03:06:13 +0800

    feat: 给Home添加组件

--- Sample-DesignCode/Home.swift
+++ Sample-DesignCode/Home.swift
@@ -11,16 +11,25 @@ import SwiftUI
 struct Home: View {
     @State var showProfile = false
     @State var viewState = CGSize.zero
+    @State var showContent = false
 
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
                 // 根视图的安全区域颜色设置为全部`.edgesIgnoringSafeArea(.all)`
                 .edgesIgnoringSafeArea(.all)
-            
-            HomeView(showProfile: $showProfile)
+
+            HomeView(showProfile: $showProfile, showContent: $showContent)
                 .padding(.top, 44)
-                .background(Color.white)
+                .background(
+                    VStack {
+                        LinearGradient(gradient: Gradient(colors: [Color("background2"), Color.white]), startPoint: .top, endPoint: .bottom)
+                            .frame(height: 200)
+                        
+                        Spacer()
+                    }
+                    .background(Color.white)
+                )
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
                 .offset(y: showProfile ? -450 : 0)
@@ -41,7 +50,7 @@ struct Home: View {
                     DragGesture().onChanged { value in
                         self.viewState = value.translation
                     }
-                    .onEnded { value in
+                    .onEnded { _ in
                         if self.viewState.height > 50 {
                             self.showProfile = false
                         }
@@ -49,6 +58,34 @@ struct Home: View {
                         self.viewState = .zero
                     }
                 )
+
+            // 使用if语句，提高view性能
+            if showContent {
+                // 在ZHack里面的任何元素将会彼此重叠
+                // 设置颜色，忽略安全区
+                Color.white.edgesIgnoringSafeArea(.all)
+                ContentView()
+
+                VStack {
+                    HStack {
+                        Spacer()
+
+                        Image(systemName: "xmark")
+                            .frame(width: 36, height: 36)
+                            // 前景色
+                            .foregroundColor(.white)
+                            .background(Color.black)
+                            .clipShape(Circle())
+                    }
+                    Spacer()
+                }
+                .offset(x: -16, y: 16)
+                .transition(.move(edge: .top))
+                .animation(.spring(response: 0.6, dampingFraction: 0.8, blendDuration: 0))
+                .onTapGesture {
+                    self.showContent = false
+                }
+            }
         }
     }
 }
@@ -62,7 +99,7 @@ struct Home_Previews: PreviewProvider {
 struct AvatarView: View {
     // 共享状态
     @Binding var showProfile: Bool
-    
+
     var body: some View {
         Button(action: { self.showProfile.toggle() }) {
             Image("Avatar")
@@ -76,4 +113,3 @@ struct AvatarView: View {
 
 // 检测屏幕的尺寸
 let screen = UIScreen.main.bounds
-
--- Sample-DesignCode/HomeView.swift
+++ Sample-DesignCode/HomeView.swift
@@ -11,77 +11,97 @@ import SwiftUI
 struct HomeView: View {
     @Binding var showProfile: Bool
     @State var showUpdate = false
+    @Binding var showContent: Bool
 
     var body: some View {
-        VStack {
-            HStack {
-                Text("Watching")
-//                    .font(.system(size: 28, weight: .bold)) // 系统默认的优先级高于自定义封装组件的优先级
-                    .modifier(CustomFontModifier(size: 28))
-                
-                Spacer()
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Watching")
+    //                    .font(.system(size: 28, weight: .bold)) // 系统默认的优先级高于自定义封装组件的优先级
+                        .modifier(CustomFontModifier(size: 28))
+                    
+                    Spacer()
 
-                AvatarView(showProfile: $showProfile)
+                    AvatarView(showProfile: $showProfile)
 
-                Button(action: { self.showUpdate.toggle() }) {
-                    Image(systemName: "bell")
-                        // 默认情况下是黑色
-                        .renderingMode(.original)
-                        .font(.system(size: 16, weight: .medium))
-                        .frame(width: 36, height: 36)
-                        .background(Color.white)
-                        .clipShape(Circle())
-                        .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
-                        .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
-                }
-                .sheet(isPresented: $showUpdate) {
-                    UpdateList()
-                }
-            }
-            .padding(.horizontal, 20)
-            .padding(.leading, 14)
-            .padding(.top, 30)
-            // HStack可以将元素对齐到顶部或底部或中间
-            // HStack视图 都将水平分布
-            // VStack可以对齐到左,中或右
-
-            // 环形进度条组件
-            ScrollView(.horizontal, showsIndicators: false) {
-                WatchRingsView()
-                    .padding(.horizontal, 30)
-                // 为了抵消阴影
-                    .padding(.bottom, 30)
-            }
-            
-            
-
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 20) {
-                    ForEach(sectionData) { item in
-                        GeometryReader { geometry in
-                            SectionView(section: item)
-                                .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -20), axis: (x: 0, y: 10.0, z: 0))
-                        }
-                        .frame(width: 275, height: 275)
+                    Button(action: { self.showUpdate.toggle() }) {
+                        Image(systemName: "bell")
+                            // 默认情况下是黑色
+                            .renderingMode(.original)
+                            .font(.system(size: 16, weight: .medium))
+                            .frame(width: 36, height: 36)
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(color: Color.black.opacity(0.1), radius: 1, x: 0, y: 1)
+                            .shadow(color: Color.black.opacity(0.2), radius: 10, x: 0, y: 10)
+                    }
+                    .sheet(isPresented: $showUpdate) {
+                        UpdateList()
                     }
                 }
-                .padding(30)
-                .padding(.bottom, 30)
-            }
+                .padding(.horizontal, 20)
+                .padding(.leading, 14)
+                .padding(.top, 30)
+                // HStack可以将元素对齐到顶部或底部或中间
+                // HStack视图 都将水平分布
+                // VStack可以对齐到左,中或右
 
-            Spacer()
+                // 环形进度条组件
+                ScrollView(.horizontal, showsIndicators: false) {
+                    WatchRingsView()
+                        .padding(.horizontal, 30)
+                    // 为了抵消阴影
+                        .padding(.bottom, 30)
+                        .onTapGesture {
+                            self.showContent = true
+                    }
+                }
+                
+                
+
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 20) {
+                        ForEach(sectionData) { item in
+                            GeometryReader { geometry in
+                                SectionView(section: item)
+                                    .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 30) / -20), axis: (x: 0, y: 10.0, z: 0))
+                            }
+                            .frame(width: 275, height: 275)
+                        }
+                    }
+                    .padding(30)
+                    .padding(.bottom, 30)
+                }
+                .offset(y: -30)
+                
+                HStack {
+                    Text("Courses")
+                        .font(.title).bold()
+                    Spacer()
+                }
+                .padding(.leading, 30)
+                .offset(y: -60)
+                
+                SectionView(section: sectionData[2], width: screen.width - 60, height: 275)
+                .offset(y: -60)
+
+                Spacer()
+            }
         }
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(showProfile: .constant(false))
+        HomeView(showProfile: .constant(false), showContent: .constant(false))
     }
 }
 
 struct SectionView: View {
     var section: Section
+    var width: CGFloat = 275
+    var height: CGFloat = 275
 
     var body: some View {
         VStack {
@@ -103,7 +123,7 @@ struct SectionView: View {
         }
         .padding(.top, 20)
         .padding(.horizontal, 20)
-        .frame(width: 275, height: 275)
+        .frame(width: width, height: height)
         .background(section.color)
         .cornerRadius(30)
         // 设置自定义阴影
