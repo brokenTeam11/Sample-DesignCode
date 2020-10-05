397c9870681bb8dc8c14c52e84c8715987548ab9
xianengqi <xianengqi@gmail.com>
2020-10-06 03:20:19 +0800

    Update CourseList.swift
    有个字母写错了，Xcode检测语法错误，我排查问题花了1个小时才找到，记住啦，别拼错了api的关键字

--- Sample-DesignCode/CourseList.swift
+++ Sample-DesignCode/CourseList.swift
@@ -11,13 +11,14 @@ import SwiftUI
 struct CourseList: View {
     @State var courses = courseData
     @State var active = false
-    
+    @State var activeIndex = -1
+
     var body: some View {
         ZStack {
             Color.black.opacity(active ? 0.5 : 0)
                 .animation(.linear)
                 .edgesIgnoringSafeArea(.all)
-            
+
             ScrollView {
                 VStack(spacing: 30) {
                     Text("Courese")
@@ -29,8 +30,17 @@ struct CourseList: View {
                     // `id: \.self`提供索引
                     ForEach(courses.indices, id: \.self) { index in
                         GeometryReader { geometry in
-                            CourseView(show: self.$courses[index].show, course: self.courses[index], active: self.$active)
+                            CourseView(
+                                show: self.$courses[index].show,
+                                course: self.courses[index],
+                                active: self.$active,
+                                index: index,
+                                activeIndex: self.$activeIndex
+                            )
                                 .offset(y: self.courses[index].show ? -geometry.frame(in: .global).minY : 0)
+                                .opacity(self.activeIndex != index && self.active ? 0 : 1)
+                                .scaleEffect(self.activeIndex != index && self.active ? 0.5 : 1)
+                                .offset(x: self.activeIndex != index && self.active ? screen.width : 0)
                         }
                         .frame(height: 280)
                         .frame(maxWidth: self.courses[index].show ? .infinity : screen.width - 60)
@@ -40,9 +50,9 @@ struct CourseList: View {
                 .frame(width: screen.width)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             }
-             // 隐藏状态栏
-                .statusBar(hidden: active ? true : false)
-                .animation(.linear)
+            // 隐藏状态栏
+            .statusBar(hidden: active ? true : false)
+            .animation(.linear)
         }
     }
 }
@@ -54,18 +64,20 @@ struct CourseList_Previews: PreviewProvider {
 }
 
 struct CourseView: View {
-    @Binding var show:Bool
+    @Binding var show: Bool
     var course: Course
     @Binding var active: Bool
+    var index: Int
+    @Binding var activeIndex: Int
 
     var body: some View {
         ZStack(alignment: .top) {
             VStack(alignment: .leading, spacing: 30.0) {
                 Text("Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.")
-                
+
                 Text("About this course")
                     .font(.title).bold()
-                
+
                 Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and building real apps for iOS and macOS. While it's not one codebase for all apps, you learn once and can apply the techniques and controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
                 Text(" controls to all platforms with incredible quality, consistency and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.Take your SwiftUI app to the App Store with advanced techniques like API data, packages and CMS.")
             }
@@ -73,13 +85,12 @@ struct CourseView: View {
             .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280, alignment: .top)
             .offset(y: show ? 460 : 0)
             .background(Color.white)
-                // 平滑角
+            // 平滑角
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
             // 添加不透明度
             .opacity(show ? 1 : 0)
-            
-            
+
             VStack {
                 HStack(alignment: .top) {
                     VStack(alignment: .leading, spacing: 8.0) {
@@ -113,17 +124,22 @@ struct CourseView: View {
             }
             .padding(show ? 30 : 20)
             .padding(.top, show ? 30 : 0)
-    //        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
+            //        .frame(width: show ? screen.width : screen.width - 60, height: show ? screen.height : 280)
             // `.infinity表示安全区域`
             .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
-                .background(Color(course.color))
+            .background(Color(course.color))
             // 平滑的圆角
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color(course.color).opacity(0.3), radius: 20, x: 0, y: 20)
-            
+
             .onTapGesture {
                 self.show.toggle()
                 self.active.toggle()
+                if self.show {
+                    self.activeIndex = self.index
+                } else {
+                    self.activeIndex = -1
+                }
             }
         }
         .frame(height: show ? screen.height : 280)
@@ -143,9 +159,7 @@ struct Course: Identifiable {
 }
 
 var courseData = [
-    Course(title: "Prototype designs in SwiftUI", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Card1"), logo: #imageLiteral(resourceName: "Logo1"),  color: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), show: false),
-    Course(title: "Build a SwiftUI App", subtitle: "22 Sections", image: #imageLiteral(resourceName: "Card3"), logo: #imageLiteral(resourceName: "Logo1"),  color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), show: false),
-    Course(title: "SwiftUI Advanced", subtitle: "30 Sections", image: #imageLiteral(resourceName: "Background1"), logo: #imageLiteral(resourceName: "Logo1"),  color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), show: false),
+    Course(title: "Prototype designs in SwiftUI", subtitle: "18 Sections", image: #imageLiteral(resourceName: "Card1"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1), show: false),
+    Course(title: "Build a SwiftUI App", subtitle: "22 Sections", image: #imageLiteral(resourceName: "Card3"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1), show: false),
+    Course(title: "SwiftUI Advanced", subtitle: "30 Sections", image: #imageLiteral(resourceName: "Background1"), logo: #imageLiteral(resourceName: "Logo1"), color: #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1), show: false),
 ]
-
- 
