ed6ff2feb492fbc5e36b86bc47e3f89ab31f3067
xianengqi <xianengqi@gmail.com>
2020-10-11 23:22:05 +0800

    Update Buttons.swift

--- Sample-DesignCode/Buttons.swift
+++ Sample-DesignCode/Buttons.swift
@@ -101,9 +101,17 @@ struct CircleButton: View {
     @State var press = false
     
     var body: some View {
-        VStack {
+        ZStack {
             Image(systemName: "sun.max")
                 .font(.system(size: 44, weight: .light))
+                .offset(x: press ? -90 : 0, y: press ? -90 : 0)
+                .rotation3DEffect(Angle(degrees: press ? 20 : 0), axis: (x: 10, y: -10, z: 0))
+            
+            Image(systemName: "moon")
+                .font(.system(size: 44, weight: .light))
+                .offset(x: press ? 0 : 90, y: press ? 0 : 90)
+                .rotation3DEffect(Angle(degrees: press ? 0 : 20), axis: (x: -10, y: 10, z: 0))
+            
         }
         .frame(width: 100, height: 100)
         .background(
@@ -123,5 +131,18 @@ struct CircleButton: View {
         .clipShape(Circle())
         .shadow(color: Color(#colorLiteral(red: 0.7608050108, green: 0.8164883852, blue: 0.9259157777, alpha: 1)), radius: 20, x: -20, y: -20)
         .shadow(color: Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), radius: 20, x: 20, y: 20)
+        .scaleEffect(tap ? 1.2 : 1)
+        .gesture(
+            LongPressGesture().onChanged { value in
+                self.tap = true
+                // 点击的延迟触发事件
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    self.tap = false
+                }
+            }
+            .onEnded { value in
+                self.press.toggle()
+            }
+        )
     }
 }
